743f72b666cf1b0e50ffd6932e78efd2a9bfbaf4
Matt Faltyn <76570855+mattfaltyn@users.noreply.github.com>
2025-03-26 10:37:30 +0100

    Add strategy for 0xd0d6_869f5f

---
+++ user-models/0xd0d6_869f5f/0xd0d6_869f5f_strategy.py
@@ -0,0 +1,22 @@
+# Stacking Sats Challenge
+# Write your Bitcoin accumulation strategy here
+
+def execute_strategy(usd_balance, btc_price):
+    """
+    Implement your Bitcoin accumulation strategy.
+    
+    Parameters:
+    - usd_balance: Current USD balance
+    - btc_price: Current Bitcoin price in USD
+    
+    Returns:
+    - usd_amount_to_spend: Amount of USD to spend on Bitcoin
+    """
+    
+    # Simple example: Spend 10% of available USD balance
+    usd_amount_to_spend = usd_balance * 0.1
+    
+    # Return the amount to spend
+    return usd_amount_to_spend
+
+# Your strategy will be executed automatically
